a6be345d1846eefd40ef78f21c1ae7c4a26492db
Wesley Ruben <wesleyruben213@hotmail.com>
2022-12-14 16:53:12 -0300

    feature: alterando baseurl e regras

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -27,14 +27,14 @@ class AgendamentoController extends Controller
         //
         // dd([$request->nome,$request->limit,$request->pesquisa]);
         if($request->pesquisa){
-            $agendamento_index = Http::get('http://login.developrt.com/api/login',[
+            $agendamento_index = Http::get('localhost:8080/api/user/all',[
 
                 'filtro' => "$request->nome:$request->pesquisa",
                 'limit' => $request->limit,
 
             ])->object();
         }else{
-            $agendamento_index = Http::get('http://login.developrt.com/api/login')->object();
+            $agendamento_index = Http::get('localhost:8080/api/user/all')->object();
 
 
         }
@@ -55,47 +55,7 @@ class AgendamentoController extends Controller
         return view('site.agendamento-create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreAgendamentoRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-        $regras = [
-            'nome' => 'required|alpha|min:4',
-            'sobrenome' => 'required|alpha|min:4',
-            'email' => 'required|email',
-            'senha' => 'required|min:4',
-        ];
-        $feedback = [
-            'required' => 'O campo :attribute e obrigatorio',
-            'alpha' => 'O campo :attribute so aceita letras',
-            'min' => 'O campo :attribute aceita no minimo 4 letras',
-            'email' => 'O campo :attribute tem que ser um Email valido',
-            'in' => 'O campo :attribute so aceita ADM OU visitante',
-
-        ];
-
-        $request->validate($regras, $feedback);
-
-        // dd([$request->all()]);
-
-        $agendamento_index = Http::post('http://login.developrt.com/api/login',[
-
-            'nome' => $request->nome,
-            'sobrenome' => $request->sobrenome,
-            'email' => $request->email,
-            'senha' => $request->senha,
-            'type' => $request->type,
-        ]);
-
-        return redirect()->route('agendamento.index');
-    }
-
-    /**
+        /**
      * Display the specified resource.
      *
      * @param  \App\Models\Agendamento  $agendamento
@@ -116,12 +76,54 @@ class AgendamentoController extends Controller
     public function edit($id)
     {
         //
-        $agendamento_edit = Http::get('http://login.developrt.com/api/login/'.$id)->object();
+        $agendamento_edit = Http::get('localhost:8080/api/user/'.$id)->object();
 
 
         // dd([$agendamento_edit]);
         return view('site.agendamento-edit', compact('agendamento_edit'));
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAgendamentoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $regras = [
+            'nome' => 'required|alpha|min:4',
+            'sobrenome' => 'required|alpha|min:4',
+            'email' => 'required|email',
+            'senha' => 'required|min:4',
+        ];
+        $feedback = [
+            'required' => 'Campo obrigatorio',
+            'alpha' => 'Apenas letras',
+            'min' => 'Minimo 4 caracteres',
+            'email' => 'Email invalido',
+            'in' => 'Apenas ADM OU visitante',
+
+        ];
+
+        $request->validate($regras, $feedback);
+
+        // dd([$request->all()]);
+
+        $agendamento_index = Http::post('localhost:8080/api/user/create',[
+
+            'nome' => $request->nome,
+            'sobrenome' => $request->sobrenome,
+            'email' => $request->email,
+            'senha' => $request->senha,
+            'type' => $request->type,
+        ]);
+
+        return redirect()->route('agendamento.index');
+    }
+
+
 
     /**
      * Update the specified resource in storage.
@@ -134,7 +136,7 @@ class AgendamentoController extends Controller
     {
 
 
-        $agendamento_update = Http::post('http://login.developrt.com/api/login/'.$id,[
+        $agendamento_update = Http::put('localhost:8080/api/user/update/'.$id,[
 
             'nome' => $request->nome,
             'sobrenome' => $request->sobrenome,
@@ -157,7 +159,7 @@ class AgendamentoController extends Controller
     public function destroy($id)
     {
 
-        $agendamento_update = Http::delete('http://login.developrt.com/api/login/'.$id);
+        $agendamento_update = Http::delete('localhost:8080/api/user/delete/'.$id);
 
         return redirect()->route('agendamento.index');
     }
